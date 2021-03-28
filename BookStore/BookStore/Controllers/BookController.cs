ddc53323697e31b605efcf90c9f01ebfd374120c
hasnainsubhani <34906377+hasnainsubhani@users.noreply.github.com>
2021-03-29 01:39:52 +0530

    Added Book Model and controller

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+	public class BookController : Controller
+	{
+		private readonly BookRepository _bookRepository = null;
+		public BookController()
+		{
+			_bookRepository = new BookRepository();
+		}
+		public List<BookModel> GetAllBook()
+		{
+			return _bookRepository.GetAllBook();
+		}
+
+		public BookModel GetBook(int id)
+		{
+			return _bookRepository.GetBookById(id);
+		}
+
+		public List<BookModel> SearchBook(string Name,string Author)
+		{
+			return _bookRepository.SearchBook(Name,Author);
+		}
+	}
+}
---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+	public class BookRepository
+	{
+		public List<BookModel> GetAllBook()
+		{
+			return DataSource();
+		}
+
+		public BookModel GetBookById(int id)
+		{
+			return DataSource().Where(x => x.id == id).FirstOrDefault();
+		}
+
+		public List<BookModel> SearchBook(string Name, string Author)
+		{
+			return DataSource().Where(x => x.title.Contains(Name) && x.author.Contains(Author)).ToList();
+		}
+
+		private List<BookModel> DataSource()
+		{
+			return new List<BookModel>() {
+				new BookModel(){id=1,title="Net Core",author="Hasnain"},
+				new BookModel(){id=2,title="Java",author="Hasnain"},
+				new BookModel(){id=3,title="Php",author="Venket"},
+				new BookModel(){id=4,title="MVC Net Core",author="Ammaar"},
+				new BookModel(){id=5,title="C#",author="Aqdus"}
+			};
+		}
+
+	}
+}
